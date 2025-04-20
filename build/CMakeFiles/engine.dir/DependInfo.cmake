c145f4acc7de056271f1f0edc3e4f19576258bfd
satoshi <alwaysindiscipline@gmail.com>
2025-04-20 01:19:42 -0300

    engine

---
+++ build/CMakeFiles/engine.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/satoshi/Documentos/workspace/engine/source/core/launcher/launcher.cpp" "CMakeFiles/engine.dir/source/core/launcher/launcher.cpp.o" "gcc" "CMakeFiles/engine.dir/source/core/launcher/launcher.cpp.o.d"
+  "/home/satoshi/Documentos/workspace/engine/source/main.cpp" "CMakeFiles/engine.dir/source/main.cpp.o" "gcc" "CMakeFiles/engine.dir/source/main.cpp.o.d"
+  "/home/satoshi/Documentos/workspace/engine/source/platform/window_x11.cpp" "CMakeFiles/engine.dir/source/platform/window_x11.cpp.o" "gcc" "CMakeFiles/engine.dir/source/platform/window_x11.cpp.o.d"
+  "" "engine" "gcc" "CMakeFiles/engine.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/engine.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/engine.dir/link.d"
+  "CMakeFiles/engine.dir/source/core/launcher/launcher.cpp.o"
+  "CMakeFiles/engine.dir/source/core/launcher/launcher.cpp.o.d"
+  "CMakeFiles/engine.dir/source/main.cpp.o"
+  "CMakeFiles/engine.dir/source/main.cpp.o.d"
+  "CMakeFiles/engine.dir/source/platform/window_x11.cpp.o"
+  "CMakeFiles/engine.dir/source/platform/window_x11.cpp.o.d"
+  "engine"
+  "engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
